a996215306415ddfb0c840c6c453dabb804a3de3
kimtienvu <kvu@ucsd.edu>
2021-02-15 11:07:03 -0800

    attempt at pop up

--- routes/chatroom.js
+++ routes/chatroom.js
@@ -4,6 +4,49 @@
  */
 
 exports.view = function(req, res){
-  res.render('chatroom');
+  res.render('chatroom', {
+    'proficiency': [
+      { 	'name': '🏫 Beginner Grammar',
+        'image': 'enter.svg',
+        'id': 'beginGram'
+      },
+      { 	'name': '🏫 Beginner Conversation',
+        'image': 'enter.svg',
+        'id': 'beginConv'
+      },
+      { 	'name': '🏫 Beginner Writing',
+        'image': 'enter.svg',
+        'id': 'beginWriting'
+      },
+      { 	'name': '🏫 Beginner Reading',
+        'image': 'enter.svg',
+        'id': 'beginRead'
+      } 	
+    ], 
+    'generalChat':[
+      { 	'name': '🌮 Foods',
+          'image': 'enter.svg',
+          'description': 'Chat about good Spanish/Mexican food',
+          'id': 'foodChat'
+      },
+      { 	'name': '✈️ Travel',
+        'image': 'enter.svg',
+        'description': 'Chat about travel stories and recommendations',
+        'id': 'travelChat'
+      },
+      { 	'name': '🖼 Arts & Culture',
+        'image': 'enter.svg',
+        'description': 'Chat about Arts and Culture',
+        'id': 'artsChat'
+      },
+      { 	'name': '🎮 Gaming',
+        'image': 'enter.svg',
+        'description': 'Chat about your favorite games',
+        'id': 'gameChat'
+      } 	
+    ]
+    }
+  
+  );
 };
 
